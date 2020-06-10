1f159da37c5d61dd071d653f57b7d4e32e3bb428
Rodrigo Lima <rodrigocdelima@outlook.com>
2020-06-10 19:46:06 -0300

    Service Layer, component registration

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.User;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
