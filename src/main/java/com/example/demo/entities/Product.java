ad1968b63ede4c421e67eacb475c14b85492d703
Rodrigo Lima <rodrigocdelima@outlook.com>
2020-06-16 16:19:05 -0300

    Many to Many association with JoinTable

--- src/main/java/com/example/demo/entities/Product.java
+++ src/main/java/com/example/demo/entities/Product.java
@@ -8,8 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "tb_product")
@@ -19,13 +24,15 @@ public class Product implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id;	
 	private String name;
 	private String description;
 	private Double price;
 	private String imgURL;
 	
-	@Transient
+	@Fetch(FetchMode.JOIN) 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
